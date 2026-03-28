4c08cb3b675ea67bfbbac848ebb4d04f49f8dbfc
rikhill1980-cmyk <rikhill1980@gmail.com>
2026-03-28 16:07:10 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./styles.css";
+export default function App() {
+  return (
+    <div>
+      <h1>Working ✅</h1>
+    </div>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
